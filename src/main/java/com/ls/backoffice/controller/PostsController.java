2e7d30f5146545634d5771b5d3fb418161f304d4
German <germansanz.id@pinapp.pe>
2023-04-12 17:04:59 -0300

    fix en RequestMapping

--- src/main/java/com/ls/backoffice/controller/PostsController.java
+++ src/main/java/com/ls/backoffice/controller/PostsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/post")
 public class PostsController {
     @GetMapping
     public String getPosts(){
