8eac54925366e0586c0cabf9aece77a7a6d1e505
German <germansanz.id@pinapp.pe>
2023-04-15 16:35:45 -0300

    test fix

--- src/test/java/com/ls/backoffice/domain/mapper/PostRequestMapperTest.java
+++ src/test/java/com/ls/backoffice/domain/mapper/PostRequestMapperTest.java
@@ -2,7 +2,7 @@ package com.ls.backoffice.domain.mapper;
 
 import com.ls.backoffice.controller.request.PostRequest;
 import com.ls.backoffice.domain.model.Post;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
